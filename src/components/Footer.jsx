2f80bd9b6070cff8aa02a1029fe2e4be5d79f910
Saeed Ali <saeedaliofficial77@gmail.com>
2025-01-31 22:06:46 +0500

    now all set

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ import './Footer.css';
 const Footer = () => {
   return (
     <footer className="footer">
-      <p>&copy; 2025 TikTok Downloader</p>
+      <p>&copy; Saeed Ali & Zain-ul-Abdin</p>
     </footer>
   );
 };
