facae994855f8506087446c6d9ad1f9fa882fd8e
Saeed Ali <saeedaliofficial77@gmail.com>
2025-01-31 23:01:16 +0500

    api encrypted

--- src/api.js
+++ src/api.js
@@ -8,7 +8,7 @@ const tiktokVideoDownloader = async (url) => {
             hd: '0',
         },
         headers: {
-            'x-rapidapi-key': 'a2edf803aemsh29848781a8efc73p1bad8bjsn4ef2f8b7b2d0',
+            'x-rapidapi-key': '',
             'x-rapidapi-host': 'tiktok-video-no-watermark2.p.rapidapi.com'
         }
     };
